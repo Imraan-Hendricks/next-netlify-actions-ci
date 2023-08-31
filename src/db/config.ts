f6bdb350ed0cd855c7c993148c26d738be012a03
Imraan-Hendricks <imraan.hndx@gmail.com>
2023-08-31 17:07:00 +0200

    chore: setup database connection

---
+++ src/db/config.ts
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+import { MONGO_URI, NODE_ENV } from '@/utils/env-utils';
+
+const options = {};
+
+if (!MONGO_URI)
+  throw new Error('Please add your Mongo URI environment variable');
+
+let client = new MongoClient(MONGO_URI, options);
+let clientPromise: Promise<MongoClient>;
+
+interface CustomGlobal {
+  _mongoClientPromise?: Promise<MongoClient>;
+}
+
+declare const global: CustomGlobal;
+
+if (NODE_ENV !== 'production') {
+  if (!global._mongoClientPromise) {
+    global._mongoClientPromise = client.connect();
+  }
+
+  clientPromise = global._mongoClientPromise;
+} else {
+  clientPromise = client.connect();
+}
+
+export async function connectDb() {
+  try {
+    const client = await clientPromise;
+    const db = client.db();
+    return db;
+  } catch (error) {
+    throw new Error('Failed to establish connection to database');
+  }
+}
---
+++ src/utils/env-utils.ts
@@ -0,0 +1,3 @@
+export const NODE_ENV = process.env.NODE_ENV as string;
+
+export const MONGO_URI = process.env.MONGO_URI as string;
